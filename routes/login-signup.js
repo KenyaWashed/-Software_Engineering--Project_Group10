b804129d289d6d4d976e552fc64c51f07a5da10e
Nguyen Tan Dong <ndong9711@gmail.com>
2025-06-20 16:52:12 +0700

    sửa lại cách dùng session, thêm get user từ session và hướng dẫn

--- routes/login-signup.js
+++ routes/login-signup.js
@@ -10,16 +10,19 @@ router.post('/api/login-user', login.userLoginValidator, login.login);
 router.post('/api/register-user', signup.userSignupValidator, signup.signup);
 router.get('/api/logout', logout.logout);
 // Route: kiểm tra session hiện tại
-router.get('/api/check-session', sessionController.checkSession);
+router.get('/api/check-session', session.checkSession);
 
 // Route cần quyền Chủ khách sạn
-router.get('/api/admin-only', sessionController.checkHotelOwner, (req, res) => {
+router.get('/api/admin-only', session.checkHotelOwner, (req, res) => {
   res.status(200).send('✅ Chào mừng Chủ khách sạn!');
 });
 
 // Route cần quyền Lễ tân
-router.get('/api/receptionist-only', sessionController.checkReceptionist, (req, res) => {
+router.get('/api/receptionist-only', session.checkReceptionist, (req, res) => {
   res.status(200).send('✅ Chào mừng Lễ tân!');
 });
+
+// Lấy thông tin user
+router.get('/api/me', session.getCurrentUser);
 
 module.exports = router;
