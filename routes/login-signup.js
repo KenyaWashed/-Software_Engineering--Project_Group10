1d6936ef7fbc60022ba372ea277448ab614f8407
Nguyen Tan Dong <ndong9711@gmail.com>
2025-06-20 18:02:49 +0700

    sửa các api trong session

--- routes/login-signup.js
+++ routes/login-signup.js
@@ -4,23 +4,13 @@ const router = express.Router();
 const signup = require("../controllers/signup");
 const login = require("../controllers/login");
 const logout = require("../controllers/logout");
-const sessionController = require('../controllers/session');
+const session = require('../controllers/session');
 
 router.post('/api/login-user', login.userLoginValidator, login.login);
 router.post('/api/register-user', signup.userSignupValidator, signup.signup);
 router.get('/api/logout', logout.logout);
 // Route: kiểm tra session hiện tại
 router.get('/api/check-session', session.checkSession);
-
-// Route cần quyền Chủ khách sạn
-router.get('/api/admin-only', session.checkHotelOwner, (req, res) => {
-  res.status(200).send('✅ Chào mừng Chủ khách sạn!');
-});
-
-// Route cần quyền Lễ tân
-router.get('/api/receptionist-only', session.checkReceptionist, (req, res) => {
-  res.status(200).send('✅ Chào mừng Lễ tân!');
-});
 
 // Lấy thông tin user
 router.get('/api/me', session.getCurrentUser);
